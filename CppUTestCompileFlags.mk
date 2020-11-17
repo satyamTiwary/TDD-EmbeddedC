e6aa57b738ca0a71d0e4053362325a693c9a42e2
Satyam tiwary <satyamtiwary@vvbiotech.com>
2020-11-17 16:35:36 +0530

    minor

--- CppUTestCompileFlags.mk
+++ CppUTestCompileFlags.mk
@@ -1,5 +1,5 @@
-# Figure out how to ask if gcc7 vs clang, gcc7 only right now
-ifeq "0" "1"
+# Some flags to quiet clang
+ifeq ($(shell $(CC) -v 2>&1 | grep -c "clang"), 1)
 CPPUTEST_WARNINGFLAGS += -Wno-unknown-warning-option
 CPPUTEST_WARNINGFLAGS += -Wno-covered-switch-default
 CPPUTEST_WARNINGFLAGS += -Wno-reserved-id-macro
@@ -8,16 +8,20 @@ CPPUTEST_WARNINGFLAGS += -Wno-documentation
 CPPUTEST_WARNINGFLAGS += -Wno-missing-noreturn
 endif
 
-CPPUTEST_WARNINGFLAGS += -Wall 
+CPPUTEST_WARNINGFLAGS += -Wall
 CPPUTEST_WARNINGFLAGS += -Werror
+CPPUTEST_WARNINGFLAGS += -Wfatal-errors
 CPPUTEST_WARNINGFLAGS += -Wswitch-default
 CPPUTEST_WARNINGFLAGS += -Wno-format-nonliteral
-CPPUTEST_WARNINGFLAGS += -Wno-sign-conversion 
-CPPUTEST_WARNINGFLAGS += -Wno-pedantic 
+CPPUTEST_WARNINGFLAGS += -Wno-sign-conversion
+CPPUTEST_WARNINGFLAGS += -Wno-pedantic
 CPPUTEST_WARNINGFLAGS += -Wno-shadow
 CPPUTEST_WARNINGFLAGS += -Wno-missing-field-initializers
 CPPUTEST_WARNINGFLAGS += -Wno-unused-parameter
-CPPUTEST_CFLAGS += -Wall
-CPPUTEST_CFLAGS += -Wstrict-prototypes
 CPPUTEST_CFLAGS += -pedantic
 CPPUTEST_CFLAGS += -Wno-missing-prototypes
+CPPUTEST_CFLAGS += -Wno-strict-prototypes
+CPPUTEST_CXXFLAGS += -Wno-c++14-compat
+CPPUTEST_CXXFLAGS += --std=c++11
+CPPUTEST_CXXFLAGS += -Wno-c++98-compat-pedantic
+CPPUTEST_CXXFLAGS += -Wno-c++98-compat
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # TDD-EmbeddedC
+
+https://github.com/jwgrenning/cpputest-starter-project
+
+Install CpuTest:
+
+```bash
+cd /close-to-your-production-code/cpputest
+autoreconf . -i
+./configure
+make tdd
+```
